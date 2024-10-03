becf68d259b9a18dc03ddd9ddd08babc90497408
melkent <melanie.s.kent@gmail.com>
2024-10-02 18:18:59 -0700

    code cleanup

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,7 @@
 const NotFound = () => {
   return(
     <div>
-      <p>
-        404 Page not found :&#40;
-      </p>
+      <p>404 Page not found :&#40;</p>
     </div>
   )
 }
--- frontend/src/utils/ProtectedRoutes.jsx
+++ frontend/src/utils/ProtectedRoutes.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 export const ProtectedRoutes = () => {
-    // TODO: add authentication logic
-    const auth = { tokenValid: true };
-    return auth.tokenValid ? <Outlet /> : <Navigate to="/login" />;
+    // TODO: implement authentication logic
+    const auth = { isTokenValid: true };
+    return auth.isTokenValid ? <Outlet /> : <Navigate to="/login" />;
 }
