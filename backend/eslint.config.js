7de87c56e3580ca43acdd191ea13b5016211b603
Rafael Park <parkrafael11@gmail.com>
2024-11-26 02:26:08 -0800

    fixed linting

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -3,13 +3,12 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { languageOptions: { globals: globals.node } },
+    { languageOptions: { globals: { ...globals.node, ...globals.jest } } },
     pluginJs.configs.recommended,
     {
         rules: {
             camelcase: ["warn", { properties: "never", ignoreDestructuring: false }],
             "max-len": ["warn", { code: 140 }],
-            "no-console": "warn",   
         },
     },
 ];
