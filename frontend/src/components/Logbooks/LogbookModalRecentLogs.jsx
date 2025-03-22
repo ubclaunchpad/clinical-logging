9c8ac0cf0697cb232b6fb419a6c1783ebebf5080
Jomari Francisco <fjomari100@gmail.com>
2025-03-22 01:16:02 -0700

    Configure table

--- frontend/src/components/Logbooks/LogbookModalRecentLogs.jsx
+++ frontend/src/components/Logbooks/LogbookModalRecentLogs.jsx
@@ -1,53 +1,65 @@
 import "./LogbookModalRecentLogs.css";
-import {
-    ArrowDownTrayIcon,
-} from "@heroicons/react/24/outline";
-import { useState, useEffect } from 'react';
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import { fetchData } from "../../utils/helpers/fetchData";
 
 export default function LogbookModalRecentLogs({ logbookID }) {
+  const { session } = useAuth();
+  const [logs, setLogs] = useState([]);
 
-    const { session } = useAuth();
-    const [logs, setLogs] = useState([]);
+  async function fetchLogs() {
+    const response = await fetchData(
+      session?.access_token,
+      `logbooks/${logbookID}/logs`
+    );
+    setLogs(response);
+    console.log(response);
+  }
 
-    async function fetchLogs() {
-        const response = await fetchData(session?.access_token, `logbooks/${logbookID}/logs`);
-        setLogs(response);
-        console.log(response);
-    }
+  useEffect(() => {
+    fetchLogs();
+  }, []);
 
-    useEffect(() => {
-        fetchLogs();
-    }, [])
-
-    return (
-        <div className="modal-recent-logs">
-            <div className="recent-logs-header">
-                <h2>Recent Logs</h2>
-                <div className="recent-logs-title-buttons">
-                    <div className="recent-log-download-button">Download <ArrowDownTrayIcon/></div>
-                    <div className="recent-log-view-more-button">View more</div>
-                </div>
-            </div>
-            <LogsTable logs={logs.slice(-10)}/>
+  return (
+    <div className="modal-recent-logs">
+      <div className="recent-logs-header">
+        <h2>Recent Logs</h2>
+        <div className="recent-logs-title-buttons">
+          <div className="recent-log-download-button">
+            Download <ArrowDownTrayIcon />
+          </div>
+          <div className="recent-log-view-more-button">View more</div>
         </div>
-    )
-
+      </div>
+      <LogsTable logs={logs.slice(-10)} />
+    </div>
+  );
 }
 
 const LogsTable = ({ logs }) => {
-    return (
-        <div className="recent-logs-table">
-            <table>
-                <tbody>
-                {logs.map((log) => (
-                    <tr key={log.id}>
-                    <td>{log.id}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="recent-logs-table">
+      <table>
+        <thead>
+          <tr>
+            <td>Select Log</td>
+            <td>Case No</td>
+            <td>Title</td>
+            <td>Surgeon</td>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log) => (
+            <tr key={log.id}>
+              <td><input type="checkbox"/></td>
+              <td>{log.case_no}</td>
+              <td>{log.title}</td>
+              <td>{log.surgeon}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
